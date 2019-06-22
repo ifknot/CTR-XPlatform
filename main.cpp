6ba3a0a8052627f35c9177483b8ae8978194a440
Jeremy Thornton <headwedge@hotmail.co.uk>
2019-06-22 19:31:22 +0100

    play with command line args

--- main.cpp
+++ main.cpp
@@ -2,6 +2,6 @@
 
 int main(int argc, char* argv[]) {
     for(auto i{0u}; i < argc; ++i) {
-        std::cout << i << " " << argv[0] << std::endl;
+        std::cout << i << " " << argv[i] << std::endl;
     }
 }
